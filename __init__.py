1a0168312e76c1ac355e4b2c2463ab8807df3dc5
Julius Kasiske <julius@kasiske.de>
2023-05-20 12:55:58 +0200

    organized code further

--- __init__.py
+++ __init__.py
@@ -1,7 +1,9 @@
 class NoPandasDataException(Exception):
-    def __init__(self) -> None:
+    def __init__(self, passed_data) -> None:
         super().__init__()
+        self.passed_data = passed_data
     
     def __str__(self) -> str:
-        return f"{self.__module__} only supports operations on Pandas DataFrames"
+        passed_type = type(self.passed_data)
+        return f"{self.__module__} only supports operations on Pandas DataFrames. The one passed is of type {passed_type}"
 
--- jkpre/selection.py
+++ jkpre/selection.py
@@ -1,16 +1,7 @@
 import pandas as pd
 import numpy as np
 from plotnine import *
-
-class NoPandasDataException(Exception):
-    def __init__(self, passed_data) -> None:
-        super().__init__()
-        self.passed_data = passed_data
-    
-    def __str__(self) -> str:
-        passed_type = type(self.passed_data)
-        return f"{self.__module__} only supports operations on Pandas DataFrames. The one passed is of type {passed_type}"
-    
+from jkpre import NoPandasDataException
 
 class Redundancy:
 
